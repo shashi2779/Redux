870958697fdc0c17b0982404352c52b9d1f97ae9
shashi2779 <yadavshashikant2779@gmail.com>
2022-06-16 02:27:55 +0530

    msg

--- mainRedux/multiple/multicomponentsandreducer/src/store.js
+++ mainRedux/multiple/multicomponentsandreducer/src/store.js
@@ -1,7 +1,7 @@
 // npm i redux react-redux
 
 import { createStore } from "redux";
-import rootReducer from "./redux/rootReducer";
+import rootReducer from "./redux/rootReducer"; //esse ballReducer,batReducer dono import huaa h
 const store = createStore(rootReducer);
 
 export default store;
