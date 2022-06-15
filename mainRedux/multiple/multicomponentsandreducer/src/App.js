fd83fadb4cbea1918d75911619bbea66f8f71e6c
shashi2779 <yadavshashikant2779@gmail.com>
2022-06-16 02:33:18 +0530

    msg2

--- mainRedux/multiple/multicomponentsandreducer/src/App.js
+++ mainRedux/multiple/multicomponentsandreducer/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Ball from './components/Ball';
 import Bat from './components/Bat';
-import { Provider } from 'react-redux';   //install react-redux -> npm i react-redux
+import { Provider } from 'react-redux';   //install react-redux -> npm i react-redux for Provider
 import store from './store';
 
 function App() {
--- mainRedux/multiple/multicomponentsandreducer/src/store.js
+++ mainRedux/multiple/multicomponentsandreducer/src/store.js
@@ -1,7 +1,7 @@
 // npm i redux react-redux
 
 import { createStore } from "redux";
-import rootReducer from "./redux/rootReducer"; //esse ballReducer,batReducer dono import huaa h
+import rootReducer from "./redux/rootReducer";  //esse ballReducer,batReducer dono import huaa h from rootReducer.js
 const store = createStore(rootReducer);
 
 export default store;
