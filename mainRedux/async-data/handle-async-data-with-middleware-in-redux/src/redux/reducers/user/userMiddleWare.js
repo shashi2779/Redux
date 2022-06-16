d8c8b3449a83bb181a4be69d1e88aff3a0bc000f
shashi2779 <yadavshashikant2779@gmail.com>
2022-06-17 01:48:23 +0530

    link msg

--- mainRedux/async-data/handle-async-data-with-middleware-in-redux/src/redux/reducers/user/userMiddleWare.js
+++ mainRedux/async-data/handle-async-data-with-middleware-in-redux/src/redux/reducers/user/userMiddleWare.js
@@ -1,7 +1,7 @@
 // dispatch mil jaata hai 
 export default async function userFetchMiddleWare(dispatch) {
     let resp = await fetch
-        ("https://jsonplaceholder.typicode.com/users/1");
+        ("https://jsonplaceholder.typicode.com/users/1");     // search -> json placeholder -> user (link se user ka data mil jayega)
     let users = await resp.json();
     dispatch({
         type: "success_users",
