5627f36a159fe3a9ae95748cb7957724c34cdfc6
shashi2779 <yadavshashikant2779@gmail.com>
2023-03-03 14:45:26 +0530

    gwr

--- Redux/reduxwithball/src/App.js
+++ Redux/reduxwithball/src/App.js
@@ -3,7 +3,11 @@ import Ball from './component/Ball';
 
 // ess provider k through hmm apni app ko redux ke sath connect kar sakte hai.
 //install & import  react-redux for Provider -> npm i react-redux
-import { Provider } from 'react-redux';   
+import { Provider } from 'react-redux';  
+
+//ess provide ko pata hi nhi hamara "state" kaha hai, kaun se store me hai ,
+//eske pas koi information nahi hai , toh information k liye hme "store" ko import karna pda.
+//ess "store" ko as a "parameter" pass kar degen "Provider" ko 
 import store from './store';
 
 function App() {
